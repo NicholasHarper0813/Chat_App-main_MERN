47ba418273ab6a6e200f86aeadc2143cb5e9db4f
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 09:31:57 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,19 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const cors = require("cors");
-dotenv.config();
-connectDB();
 const app = express();
 
-//Since we’re sending data from the frontend to the server, we need to parse it to JSON and specify its format.
+dotenv.config();
+connectDB();
+
 app.use(express.json());
-
 app.use(cors());
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 app.use("/api/user", userRoutes);
-
-// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
